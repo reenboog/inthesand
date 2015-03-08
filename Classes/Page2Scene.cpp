b637f64437fdc4baeeb6c5fdd71b054ffbdef608
reenboog <alex.gievsky@gmail.com>
2015-03-08 22:53:33 +0600

    Painted the reading screen.

---
+++ Classes/Page2Scene.cpp
@@ -0,0 +1,9 @@
+//
+//  Page2Scene.cpp
+//  inthesand
+//
+//  Created by Alex Gievsky on 08.03.15.
+//
+//
+
+#include "Page2Scene.h"
---
+++ Classes/Page2Scene.h
@@ -0,0 +1,14 @@
+//
+//  Page2Scene.h
+//  inthesand
+//
+//  Created by Alex Gievsky on 08.03.15.
+//
+//
+
+#ifndef __inthesand__Page2Scene__
+#define __inthesand__Page2Scene__
+
+#include <stdio.h>
+
+#endif /* defined(__inthesand__Page2Scene__) */
